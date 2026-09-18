45bd347e4c9a40c6306075f8a58a32740a9dd9b3
Ishani Basu <basuishani.2020@gmail.com>
2026-09-19 00:35:49 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int singleNo = 0;
+        for(int i : nums){
+            singleNo = singleNo ^ i;
+        }
+        return singleNo;
+    }
+}
